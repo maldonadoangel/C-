5e14a5dc72986ff05565fec50807722cf47ae547
Angel Morales <maldonadoangel7069@gmail.com>
2023-02-27 04:23:15 -0600

    Comparar cadenas de caracteres

---
+++ 3_ejercicio_cadenas.cpp
@@ -0,0 +1,17 @@
+/*Comparar cadenas - Funcion strcmp()*/
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    char palabra[] = "Hola";
+    char palabra2[] = "Hola";
+
+    if(strcmp(palabra,palabra2) == 0){
+        cout<<"Ambas cadenas son iguales"<<endl;
+    }
+    
+    return 0;
+}
