11dc4e471d6e6217f8cd624ed69be8f7efa45f15
Angel Morales <maldonadoangel7069@gmail.com>
2023-04-12 16:25:00 -0600

    Funciones en c++

--- Funciones.cpp
+++ Funciones.cpp
@@ -2,10 +2,32 @@
 
 
 using namespace std;
+//prototipo de funcion
+int numeroMayor(int x, int y);
 
 int main(){
+    int numero1, numero2 = 0;
 
+    cout<<"Ingresa tu primer numero: ";
+    cin>>numero1;
+    cout<<"Ingresa tu segundo numero: ";
+    cin>>numero2;
+
+    int numeroMax = numeroMayor(numero1, numero2);
+    cout<<"El numero mayor es: "<<numeroMax<<endl;
     
 
     return 0;
+}
+
+
+//Definicion de funcion
+
+int numeroMayor(int x, int y){
+    
+    if(x > y){
+        return x;
+    }else{
+        return y;
+    }
 }
